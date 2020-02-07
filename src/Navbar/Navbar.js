2a5e88521e76598a8ac2268458e64e04000899f6
Leired7 <leirediez.7@gmail.com>
2020-02-07 07:35:57 +0100

    navbar y quotecard

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css'
+
+function Navbar (){
+    return(
+    <div className="navbar">
+    <a href="https://en.wikipedia.org/wiki/The_Simpsons">Simpons Quotes</a>
+    </div>
+    );
+}
+
+export default Navbar;
