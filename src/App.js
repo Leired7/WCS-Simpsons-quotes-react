6fda1589408aa9c95ce6b5110eb0fd60e7cde76e
Leired7 <leirediez.7@gmail.com>
2020-02-08 06:22:42 +0100

    mitad de travesía Quest2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
-import Counter from './components/Counter'
+import QuoteCard from './components/QuoteCard';
+import Navbar from  './Navbar/Navbar'
+
 
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <div className="Navbar">
+      <Navbar />
       </div>
+      <div>
+        <div className="QuoteCard">
+          <div className="tablon">
+            <QuoteCard />
+            <QuoteCard />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
